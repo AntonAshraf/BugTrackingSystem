ae9ecce73fd63c7763fd11c9940c96545084c47a
Anton Ashraf <51335125+AntonAshraf@users.noreply.github.com>
2023-05-22 09:42:29 +0300

    Create Bug.java

---
+++ src/Bug.java
@@ -0,0 +1,50 @@
+package system;
+
+
+public class Bug{
+	
+	private int bugId;
+	private String name;
+	private String type;
+	private String priority;
+	private String projectName;
+	private String date;
+	private String status;
+	private int testerID;
+	private int developerID;
+	
+//	public int genID() {
+//		return 
+//	}
+	
+	public Bug(int bugId, String name, String type, String priority, String projectName,int testerID) {
+    
+		this.bugId = bugId;
+		this.name = name;
+		this.type = type;
+		this.priority = priority;
+		this.projectName = projectName;
+		this.date = Date.getCurrentDate();
+		this.status = "Opened";
+		this.testerID = testerID;
+//		this.developerID = null;
+	}
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public void setDeveloperID(int developerID) {
+		this.developerID = developerID;
+	}
+
+	public String getBugStatus() {
+		return "Bug [bugId=" + bugId + ", name=" + name + ", type=" + type + ", priority=" + priority + ", projectName="
+				+ projectName + ", date=" + date + ", status=" + status + ", testerID=" + testerID + ", developerID="
+				+ developerID + "]";
+	}
+	
+}
