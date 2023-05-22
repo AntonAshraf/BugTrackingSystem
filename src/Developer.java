b468a003064b20e646eb3629d41c9002314d07c0
Ahmed Kamal <71941689+AhmadKamal0@users.noreply.github.com>
2023-05-22 21:00:20 +0300

    Update Developer.java

--- src/Developer.java
+++ src/Developer.java
@@ -1,9 +1,12 @@
+package system;
 
 import java.util.ArrayList;
 
 public class Developer extends User {
 	
 	private ArrayList<Bug> Assigned_bugs;
+	private int num_of_closed_bugs;
+	private int num_of_reopened_bugs;
 	
 	public Developer(int id, String name, String email, String password,ArrayList<Bug> Assigned_bugs)
 	
@@ -23,12 +26,29 @@ public class Developer extends User {
 	}
 	public void change_bug_status(Bug bug, String status)
 	{
-		
-		
-		bug.setStatus(status);
+		if (status=="opened" || status=="closed") {
+			bug.setStatus(status);
+			
+			
+			String s = bug.getBugStatus();
+			if (s == "opened" && status == "closed")
+			{
+				num_of_closed_bugs++;
+				
+			}if(s == "closed" && status == "opend") {
+				num_of_reopened_bugs++;
+				num_of_closed_bugs--;
+			}
+			
+			
+		}else {
+			System.out.print("Wrong Bug Status");
+		}
 		
 	}
-	
+	public double performance(){
+		return num_of_closed_bugs - num_of_closed_bugs;
+	}
 	
 
 }
