8f17502035440305228b4a3c2a5b92f650235e61
IsmailFakhr <98194133+IsmailFakhr@users.noreply.github.com>
2023-05-28 01:12:48 +0300

    Update AuthGUI.java

--- src/GUI/AuthGUI.java
+++ src/GUI/AuthGUI.java
@@ -1,293 +1,227 @@
 package GUI;
 
 import Authentication.Auth;
-
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
-
-
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPasswordField;
+import javax.swing.JComboBox;
 
 
 public class AuthGUI extends JFrame{
+  private JTextField SignUptxtName;
+  private JTextField SignUptxtId;
+  private JTextField SignUptxtPassword;
+  private JTextField SignUptxtEmail;
+  private JLabel SignUptxtEnterYourName;
+  private JLabel SignUptxtEnterYourId;
+  private JLabel SignUptxtEnterYourPassword;
+  private JLabel SignUptxtEnterYourEmail;
+  private JLabel SignUptxtEnterYourRole;
+  private JTextField LogIntxtEmail;
+  private JTextField LogIntxtPassword;
+  private JLabel LogIntxtEnterYourEmail;
+  private JLabel LogIntxtEnterYourPassword;
+  private JLabel LogIntxtEnterYourRole;
 
-  private JTextField adtxtName;
-  private JTextField adtxtId;
-  private JTextField adtxtPhone;
-  private JTextField adtxtEmail;
-  private JTextField adtxtEnterYourName;
-  private JTextField adtxtEnterYourAge;
-  private JTextField adtxtEnterYourId;
-  private JTextField adtxtEnterYourEmail;
-  private JTextField dvltxtName;
-  private JTextField dvltxtId;
-  private JTextField dvltxtEnterYourName;
-  private JTextField dvltxtEnterYourAge;
- 
+  public void signup() {
+    JFrame SignUpFrame = new JFrame("Sign Up");
 
-  private void signupPage(String userType, String Table) {
+    SignUpFrame.setLocation(710, 340);
+    SignUpFrame.setSize(500, 400);
+    SignUpFrame.setVisible(true);
+    SignUpFrame.setResizable(false);
+    SignUpFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    SignUpFrame.getContentPane().setLayout(null);
+    
+    	
+    SignUptxtName = new JTextField();
+    SignUptxtName.setBounds(25, 35, 200, 20);
+    SignUpFrame.getContentPane().add(SignUptxtName);
+    SignUptxtName.setColumns(10);
 
-    JFrame adFrame = new JFrame(userType + " Sign Up");
+    SignUptxtId = new JTextField();
+    SignUptxtId.setBounds(25, 85, 200, 20);
+    SignUpFrame.getContentPane().add(SignUptxtId);
+    SignUptxtId.setColumns(10);
 
-    adFrame.setSize(400, 300);
-    adFrame.setVisible(true);
+    SignUptxtPassword = new JPasswordField();
+    SignUptxtPassword.setBounds(25, 135, 200, 20);
+    SignUpFrame.getContentPane().add(SignUptxtPassword);
+    SignUptxtPassword.setColumns(10);
 
-    adFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+    SignUptxtEmail = new JTextField();
+    SignUptxtEmail.setBounds(25, 185, 200, 20);
+    SignUpFrame.getContentPane().add(SignUptxtEmail);
+    SignUptxtEmail.setColumns(10);
 
-    adFrame.getContentPane().setLayout(null);
+    JButton btnSubmit = new JButton("Sign Up");
+    btnSubmit.setBounds(350, 300, 90, 25);
+    SignUpFrame.getContentPane().add(btnSubmit);
 
-    adtxtName = new JTextField();
-    adtxtName.setToolTipText("");
-    adtxtName.setBounds(225, 11, 86, 20);
-    adFrame.getContentPane().add(adtxtName);
-    adtxtName.setColumns(10);
+    SignUptxtEnterYourName = new JLabel();
+    SignUptxtEnterYourName.setText("Enter your Name:");
+    SignUptxtEnterYourName.setBounds(25, 10, 133, 20);
+    SignUpFrame.getContentPane().add(SignUptxtEnterYourName);
 
-    adtxtId = new JTextField();
-    adtxtId.setHorizontalAlignment(JTextField.LEFT);
-    adtxtId.setBounds(225, 54, 86, 20);
-    adFrame.getContentPane().add(adtxtId);
-    adtxtId.setColumns(10);
+    SignUptxtEnterYourId = new JLabel();
+    SignUptxtEnterYourId.setText("Enter your ID:");
+    SignUptxtEnterYourId.setBounds(25, 60, 133, 20);
+    SignUpFrame.getContentPane().add(SignUptxtEnterYourId);
 
-    adtxtPhone = new JTextField();
-    adtxtPhone.setBounds(225, 95, 86, 20);
-    adFrame.getContentPane().add(adtxtPhone);
-    adtxtPhone.setColumns(10);
+    SignUptxtEnterYourPassword = new JLabel();
+    SignUptxtEnterYourPassword.setText("Enter your Password :");
+    SignUptxtEnterYourPassword.setBounds(25, 110, 133, 20);
+    SignUpFrame.getContentPane().add(SignUptxtEnterYourPassword);
 
-    adtxtEmail = new JTextField();
-    adtxtEmail.setBounds(225, 143, 86, 20);
-    adFrame.getContentPane().add(adtxtEmail);
-    adtxtEmail.setColumns(10);
+    SignUptxtEnterYourEmail = new JLabel();
+    SignUptxtEnterYourEmail.setText("Enter your E-mail:");
+    SignUptxtEnterYourEmail.setBounds(25, 160, 133, 20);
+    SignUpFrame.getContentPane().add(SignUptxtEnterYourEmail);
+    
+    SignUptxtEnterYourRole = new JLabel();
+    SignUptxtEnterYourRole.setText("Choose your Role:");
+    SignUptxtEnterYourRole.setBounds(25, 210, 133, 20);
+    SignUpFrame.getContentPane().add(SignUptxtEnterYourRole);
+    
+    String[] UserOptions = {"Tester", "Developer", "Project Manager", "Admin"};
 
-    JButton btnSubmit = new JButton("Submit");
-    btnSubmit.setBounds(145, 218, 104, 23);
-    adFrame.getContentPane().add(btnSubmit);
-
-    adtxtEnterYourName = new JTextField();
-    adtxtEnterYourName.setText("Enter your name:");
-    adtxtEnterYourName.setEditable(false);
-    adtxtEnterYourName.setBounds(25, 11, 133, 20);
-    adFrame.getContentPane().add(adtxtEnterYourName);
-    adtxtEnterYourName.setColumns(10);
-
-    adtxtEnterYourAge = new JTextField();
-    adtxtEnterYourAge.setText("Enter your id:");
-    adtxtEnterYourAge.setEditable(false);
-    adtxtEnterYourAge.setColumns(10);
-    adtxtEnterYourAge.setBounds(25, 54, 133, 20);
-    adFrame.getContentPane().add(adtxtEnterYourAge);
-
-    adtxtEnterYourId = new JTextField();
-    adtxtEnterYourId.setText("Enter your password :");
-    adtxtEnterYourId.setEditable(false);
-    adtxtEnterYourId.setColumns(10);
-    adtxtEnterYourId.setBounds(25, 95, 133, 20);
-    adFrame.getContentPane().add(adtxtEnterYourId);
-
-    adtxtEnterYourEmail = new JTextField();
-    adtxtEnterYourEmail.setText("Enter your email:");
-    adtxtEnterYourEmail.setEditable(false);
-    adtxtEnterYourEmail.setColumns(10);
-    adtxtEnterYourEmail.setBounds(25, 143, 133, 20);
-    adFrame.getContentPane().add(adtxtEnterYourEmail);
+    JComboBox UserList = new JComboBox(UserOptions);
+    UserList.setSelectedIndex(0);
+    UserList.setBounds(25, 235, 200, 30);
+    SignUpFrame.getContentPane().add(UserList);
+    
+    
 
     btnSubmit.addActionListener(new ActionListener() {
       public void actionPerformed(ActionEvent e) {
-        String name = adtxtName.getText();
-        String id = adtxtId.getText();
-        String password = adtxtPhone.getText();
-        String email = adtxtEmail.getText();
+    	String name = SignUptxtName.getText();
+        String id = SignUptxtId.getText();
+        String password = SignUptxtPassword.getText();
+        String email = SignUptxtEmail.getText();
+        String role = (String) UserList.getSelectedItem();
+        String Table = "";
+        String userType = "";
+        
+        if(role == "Tester") {
+        	Table = "Testers";
+        	userType = "Tester";
+        }else if(role == "Developer") {
+        	Table = "Developers";
+        	userType = "Developer";
+        }else if(role == "Project Manager") {
+        	Table = "ProjectManagers";
+        	userType = "Project Manager";
+        }else if(role == "Admin") {
+        	Table = "Admins";
+        	userType = "Admin";
+        }
 
         Boolean x = Auth.insertData(name, id, password, email, Table);
         if (x) {
-
+          SignUpFrame.dispose();
           System.out.println("Data inserted successfully!");
           UserGUI.UserPage(userType, name, id, email);
 
         } else {
-          JButton btnSubmittt = new JButton("id already exists or invalid id try again");
-          btnSubmittt.setBounds(125, 180, 174, 23);
-          adFrame.getContentPane().add(btnSubmittt);
+        	JOptionPane.showMessageDialog(SignUpFrame, "An Error has Occured.","Error",JOptionPane.WARNING_MESSAGE);
+
         }
-        // Clear the text fields after submitting
-        adtxtName.setText("");
-        adtxtId.setText("");
-        adtxtPhone.setText("");
-        adtxtEmail.setText("");
+        SignUptxtName.setText("");
+        SignUptxtId.setText("");
+        SignUptxtPassword.setText("");
+        SignUptxtEmail.setText("");
       }
     });
   }
 
-  private void loginPage (String userType, String Table) {
-    JFrame dvlFrame = new JFrame(userType + " Login");
-        // Customize the "A" frame here if needed
-
-        dvlFrame.setSize(400, 300);
-        dvlFrame.setVisible(true);
-
-        dvlFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-
-        dvlFrame.getContentPane().setLayout(null);
-
-        dvltxtName = new JTextField();
-        dvltxtName.setToolTipText("");
-        dvltxtName.setBounds(225, 11, 86, 20);
-        dvlFrame.getContentPane().add(dvltxtName);
-        dvltxtName.setColumns(10);
-
-        dvltxtId = new JTextField();
-        dvltxtId.setHorizontalAlignment(JTextField.LEFT);
-        dvltxtId.setBounds(225, 54, 86, 20);
-        dvlFrame.getContentPane().add(dvltxtId);
-        dvltxtId.setColumns(10);
-
-        dvltxtEnterYourName = new JTextField();
-        dvltxtEnterYourName.setText("Enter your email:");
-        dvltxtEnterYourName.setEditable(false);
-        dvltxtEnterYourName.setBounds(25, 11, 133, 20);
-        dvlFrame.getContentPane().add(dvltxtEnterYourName);
-        dvltxtEnterYourName.setColumns(10);
-
-        dvltxtEnterYourAge = new JTextField();
-        dvltxtEnterYourAge.setText("Enter your password:");
-        dvltxtEnterYourAge.setEditable(false);
-        dvltxtEnterYourAge.setColumns(10);
-        dvltxtEnterYourAge.setBounds(25, 54, 133, 20);
-        dvlFrame.getContentPane().add(dvltxtEnterYourAge);
-
-        JButton btnSubmitdvl = new JButton("Submit");
-        btnSubmitdvl.setBounds(145, 218, 104, 23);
-        dvlFrame.getContentPane().add(btnSubmitdvl);
-
-        btnSubmitdvl.addActionListener(new ActionListener() {
-          @Override
-          public void actionPerformed(ActionEvent e) {
-            String email = dvltxtName.getText();
-            String password = dvltxtId.getText();
-
-            // Execute database query to check if email and password exist
-            boolean found = Auth.authenticateUser(email, password, Table);
-
-            // Display result based on whether email and password were found
-            if (found) {
-              System.out.println("Found");
-              UserGUI.UserPage(userType,"null", "-1", email);
-            } else {
-              System.out.println("Not found");
-              JButton btnSubmitdvll = new JButton("Invalid email or password try again");
-              btnSubmitdvll.setBounds(100, 118, 204, 23);
-              dvlFrame.getContentPane().add(btnSubmitdvll);
-            }
-          }
-        });
-
-  }
-  
-
-  public void signup() {
-    JFrame upFrame = new JFrame("sign up");
-
-    upFrame.setSize(500, 500);
-    upFrame.setVisible(true);
-
-    upFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-
-    upFrame.getContentPane().setLayout(null);
-
-    JButton btndev = new JButton("Developer");
-    btndev.setBounds(10, 94, 130, 23);
-    upFrame.getContentPane().add(btndev);
-
-    JButton btntst = new JButton("Tester");
-    btntst.setBounds(300, 94, 130, 23);
-    upFrame.getContentPane().add(btntst);
-
-    JButton btnpro = new JButton("project manager");
-    btnpro.setBounds(10, 194, 130, 23);
-    upFrame.getContentPane().add(btnpro);
-
-    JButton btnad = new JButton("admin");
-    btnad.setBounds(300, 194, 130, 23);
-    upFrame.getContentPane().add(btnad);
-
-    btndev.addActionListener(new ActionListener() {
-      public void actionPerformed(ActionEvent e) {
-        signupPage("Developer", "Developers");
-      }
-    });
-
-    btnad.addActionListener(new ActionListener() {
-      public void actionPerformed(ActionEvent e) {
-        signupPage("Admin", "Admins");
-      }
-    });
-
-    btnpro.addActionListener(new ActionListener() {
-      public void actionPerformed(ActionEvent e) {
-        signupPage("Project Manager", "ProjectManagers");
-      }
-    });
-
-    btntst.addActionListener(new ActionListener() {
-      public void actionPerformed(ActionEvent e) {
-        signupPage("Tester", "Testers");
-      }
-    });
-  }
-
+ 
   public void login() {
-    JFrame logFrame = new JFrame("login");
-    // Customize the "A" frame here if needed
+    JFrame LogInFrame = new JFrame("Login");
+    
+    LogInFrame.setLocation(710, 340);
+    LogInFrame.setSize(500, 400);
+    LogInFrame.setVisible(true);
+    LogInFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    LogInFrame.setResizable(false);
+    LogInFrame.getContentPane().setLayout(null);
+    
+    LogIntxtEmail = new JTextField();
+    LogIntxtEmail.setBounds(25, 35, 200, 20);
+    LogInFrame.getContentPane().add(LogIntxtEmail);
+    LogIntxtEmail.setColumns(10);
+    
+    LogIntxtPassword = new JPasswordField();
+    LogIntxtPassword.setBounds(25, 85, 200, 20);
+    LogInFrame.getContentPane().add(LogIntxtPassword);
+    LogIntxtPassword.setColumns(10);
 
-    logFrame.setSize(500, 500);
-    logFrame.setVisible(true);
+    JButton btnSubmitdvl = new JButton("Log In");
+    btnSubmitdvl.setBounds(350, 300, 90, 25);
+    LogInFrame.getContentPane().add(btnSubmitdvl);
 
-    logFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+    LogIntxtEnterYourEmail = new JLabel();
+    LogIntxtEnterYourEmail.setText("Enter your E-mail:");
+    LogIntxtEnterYourEmail.setBounds(25, 10, 133, 20);
+    LogInFrame.getContentPane().add(LogIntxtEnterYourEmail);
+    
+    LogIntxtEnterYourPassword = new JLabel();
+    LogIntxtEnterYourPassword.setText("Enter your Password :");
+    LogIntxtEnterYourPassword.setBounds(25, 60, 133, 20);
+    LogInFrame.getContentPane().add(LogIntxtEnterYourPassword);
+    
+    LogIntxtEnterYourRole = new JLabel();
+    LogIntxtEnterYourRole.setText("Choose your Role:");
+    LogIntxtEnterYourRole.setBounds(25, 110, 133, 20);
+    LogInFrame.getContentPane().add(LogIntxtEnterYourRole);
+    
+    String[] UserOptions = {"Tester", "Developer", "Project Manager", "Admin"};
 
-    logFrame.getContentPane().setLayout(null);
+    JComboBox UserList = new JComboBox(UserOptions);
+    UserList.setSelectedIndex(0);
+    UserList.setBounds(25, 135, 200, 30);
+    LogInFrame.getContentPane().add(UserList);
+    
+    btnSubmitdvl.addActionListener(new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+          String email = LogIntxtEmail.getText();
+          String password = LogIntxtPassword.getText();
+          String role = (String) UserList.getSelectedItem();
+          String Table = "";
+          String userType = "";
+          
+          if(role == "Tester") {
+          	Table = "Testers";
+          	userType = "Tester";
+          }else if(role == "Developer") {
+          	Table = "Developers";
+          	userType = "Developer";
+          }else if(role == "Project Manager") {
+          	Table = "ProjectManagers";
+          	userType = "Project Manager";
+          }else if(role == "Admin") {
+          	Table = "Admins";
+          	userType = "Admin";
+          }
 
-    JButton btnProjectManager = new JButton("project manager");
-    btnProjectManager.setBounds(10, 94, 130, 23);
-    logFrame.getContentPane().add(btnProjectManager);
+          boolean found = Auth.authenticateUser(email, password, Table);
 
-    JButton btnadmin = new JButton("admin");
-    btnadmin.setBounds(339, 94, 130, 23);
-    logFrame.getContentPane().add(btnadmin);
-
-    JButton btndevl = new JButton("developer");
-    btndevl.setBounds(10, 194, 130, 23);
-    logFrame.getContentPane().add(btndevl);
-
-    JButton btntstl = new JButton("tester");
-    btntstl.setBounds(339, 194, 130, 23);
-    logFrame.getContentPane().add(btntstl);
-
-    btntstl.addActionListener(new ActionListener() {
-      public void actionPerformed(ActionEvent e) {
-       loginPage("Tester", "Testers");
-      }
-    });
-
-    btndevl.addActionListener(new ActionListener() {
-      public void actionPerformed(ActionEvent e) {
-       loginPage("Developer", "Developers");
-      }
-    });
-
-    btnProjectManager.addActionListener(new ActionListener() {
-      public void actionPerformed(ActionEvent e) {
-       loginPage("Project Manager", "ProjectManagers");
-      }
-    });
-
-    btnadmin.addActionListener(new ActionListener() {
-      public void actionPerformed(ActionEvent e) {
-        loginPage("Admin", "Admins");
-      }  
-    });
-  }
-
-
+          if (found) {
+        	LogInFrame.dispose();
+        	System.out.println("Found");
+            UserGUI.UserPage(userType,"null", "-1", email);
+          } else {
+            System.out.println("Not found");
+            JOptionPane.showMessageDialog(LogInFrame, "An Error has Occured.","Error",JOptionPane.WARNING_MESSAGE);
+          }
+          LogIntxtEmail.setText("");
+          LogIntxtPassword.setText("");
+        }
+      });
+  	}
 }
