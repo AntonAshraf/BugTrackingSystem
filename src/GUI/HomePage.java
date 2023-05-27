c10021a87a2b0560fe98da9fe7627a25f530e3db
IsmailFakhr <98194133+IsmailFakhr@users.noreply.github.com>
2023-05-28 01:12:19 +0300

    Update HomePage.java

--- src/GUI/HomePage.java
+++ src/GUI/HomePage.java
@@ -1,44 +1,83 @@
 package GUI;
 
 import javax.swing.JFrame;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.JLabel;
 import javax.swing.border.EmptyBorder;
 import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 
 public class HomePage extends JFrame{
   
-  private JPanel contentPane;
+  //private JPanel contentPane;
 
   public void homePage() {
     AuthGUI auth = new AuthGUI();
+    
+    //setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    //setBounds(710, 340, 500, 400);
+    //contentPane = new JPanel();
+    //contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
-    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    setBounds(100, 100, 514, 356);
-    contentPane = new JPanel();
-    contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+    //setContentPane(contentPane);
+    //contentPane.setLayout(null);
+    
+    JFrame homeFrame = new JFrame("Main Menu");
+    
+    homeFrame.setLocation(710, 340);
+    homeFrame.setSize(500, 400);
+    homeFrame.setVisible(true);
+    homeFrame.setResizable(false);
+    homeFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    homeFrame.getContentPane().setLayout(null);
+    
+    ImageIcon background = new ImageIcon("C:\\Users\\PC\\Desktop\\College\\project oop\\bg.png");
+    JLabel bgLabel = new JLabel(background);
+    bgLabel.setSize(500,400);
+    bgLabel.setLocation(0,0);
+    homeFrame.add(bgLabel);
+    
 
-    setContentPane(contentPane);
-    contentPane.setLayout(null);
+    ImageIcon signinicon = new ImageIcon("C:\\Users\\PC\\Desktop\\College\\project oop\\signup.png");
+    JButton btnsignup = new JButton(signinicon);
+    btnsignup.setOpaque(false);
+    btnsignup.setContentAreaFilled(false);
+    btnsignup.setBorderPainted(false);
+    btnsignup.setBounds(140, 100, 200, 38);
+    bgLabel.add(btnsignup);
 
-    JButton btnsignup = new JButton("sign up");
-    btnsignup.setBounds(176, 94, 130, 23);
-    contentPane.add(btnsignup);
-
-    JButton btnlogin = new JButton("login");
-    btnlogin.setBounds(176, 194, 130, 23);
-    contentPane.add(btnlogin);
+    ImageIcon loginicon = new ImageIcon("C:\\Users\\PC\\Desktop\\College\\project oop\\login.png");
+    JButton btnlogin = new JButton(loginicon);
+    btnlogin.setOpaque(false); //Remove Opacity
+    btnlogin.setContentAreaFilled(false); //Remove filled background
+    btnlogin.setBorderPainted(false); //Remove Button border
+    btnlogin.setBounds(140, 150, 200, 38);
+    bgLabel.add(btnlogin);
+    
+    ImageIcon infoicon = new ImageIcon("C:\\Users\\PC\\Desktop\\College\\project oop\\info.png");
+    JButton btninfo = new JButton(infoicon);
+    btninfo.setOpaque(false); //Remove Opacity
+    btninfo.setContentAreaFilled(false); //Remove filled background
+    btninfo.setBorderPainted(false); //Remove Button border
+    btninfo.setBounds(140, 200, 200, 38);
+    bgLabel.add(btninfo);
 
     btnlogin.addActionListener(new ActionListener() {
       public void actionPerformed(ActionEvent e) {
-        auth.login();
+    	  homeFrame.dispose();
+    	  auth.login();
       }
     });
 
     btnsignup.addActionListener(new ActionListener() {
       public void actionPerformed(ActionEvent e) {
-        auth.signup();
+    	  homeFrame.dispose();
+    	  auth.signup();
       }
     });
 
